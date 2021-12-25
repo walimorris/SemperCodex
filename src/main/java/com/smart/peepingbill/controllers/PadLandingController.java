8b20c03c93300a252dcf02e4f729b80f1e6d11e9
Wali Morris <walimmorris@gmail.com>
2021-12-24 21:08:11 -0800

    add task in network system build task on PadLandingController.java

--- src/main/java/com/smart/peepingbill/controllers/PadLandingController.java
+++ src/main/java/com/smart/peepingbill/controllers/PadLandingController.java
@@ -1,6 +1,5 @@
 package com.smart.peepingbill.controllers;
 
-import com.smart.peepingbill.models.DeviceNode;
 import com.smart.peepingbill.models.SudoPopupWindow;
 import com.smart.peepingbill.models.impl.DeviceNodeImpl;
 import com.smart.peepingbill.models.impl.SmartSystemNetworkImpl;
@@ -9,6 +8,7 @@ import com.smart.peepingbill.util.NetworkUtil;
 import com.smart.peepingbill.util.constants.PeepingConstants;
 import javafx.application.ConditionalFeature;
 import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -16,6 +16,7 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -57,6 +58,7 @@ public class PadLandingController implements Initializable {
     private Button popupSubmit;
 
     private boolean is3DSupported;
+    private int deviceCount;
     private String[] ipHostArray;
     private Map<String, String> macAddresses;
     private SudoPopupWindow sudoPopupWindow;
@@ -66,6 +68,7 @@ public class PadLandingController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         sudo = new PasswordField();
+        deviceCount = 0;
         popupSubmit = new Button(PeepingConstants.SUBMIT);
         is3DSupported = Platform.isSupported(ConditionalFeature.SCENE3D);
     }
@@ -86,46 +89,72 @@ public class PadLandingController implements Initializable {
             sudoPopupWindow.getSudoFromUser();
 
             if (sudoPopupWindow.isSudoSet()) {
+                initBuildSmartSystemJsonData();
+
+                // build 2d smart system network ui
+            }
+        });
+    }
+
+    private void resetDeviceCount() {
+        deviceCount = 0;
+    }
+
+    private void parseSmartSystemNetworkJsonData(String sudo) {
+        JSONObject smartSystemJson = new JSONObject();
+
+        for (String key : macAddresses.keySet()) {
+            if (StringUtils.equals(macAddresses.get(key), ipHostArray[0])) {
+                initDeviceNode(ipHostArray[1], ipHostArray[0], key, sudo, true, smartSystemJson);
+            } else {
+                initDeviceNode(PeepingConstants.DEVICE_SMART_NODE + deviceCount,
+                        macAddresses.get(key), key, sudo, false, smartSystemJson);
+                deviceCount = deviceCount + 1;
+            }
+        }
+        // reset device count for follow-on system builds on same pad instance
+        resetDeviceCount();
+        smartSystemNetwork = new SmartSystemNetworkImpl(smartSystemJson);
+        System.out.println(smartSystemNetwork.getSmartSystemJSON());
+    }
+
+    private void initDeviceNode(String deviceType, String ipaddress, String mac, String key, boolean isHost,
+                                         JSONObject smartSystemJsonObject) {
+        DeviceNodeImpl node = new DeviceNodeImpl(deviceType, ipaddress, mac, key, isHost);
+        System.out.println(node.getNmapScanResponse());
+        if (isHost) {
+            smartSystemJsonObject.put(PeepingConstants.HOST_SMART_NODE, node.getSmartDeviceJsonObject());
+        } else {
+            smartSystemJsonObject.put(PeepingConstants.DEVICE_SMART_NODE + deviceCount, node.getSmartDeviceJsonObject());
+        }
+        node.voidSudo();
+    }
+
+    public void initBuildSmartSystemJsonData() {
+        ProgressBar bar = new ProgressBar();
+        gridpaneCenter.getChildren().add(bar);
+
+        Task<Void> task = new Task<>() {
+            @Override
+            protected Void call() {
+                // disable while task is in progress
+                buildNetworkButton.setDisable(true);
                 ipHostArray = new String[]{NetworkUtil.getIpaddress(), NetworkUtil.getHost()};
                 macAddresses = NetworkUtil.getLanDeviceMacAndIpAddresses(sudoPopupWindow.getSudo());
 
                 parseSmartSystemNetworkJsonData(sudoPopupWindow.getSudo());
                 sudoPopupWindow.voidSudo();
-
-                // build 2d smart system network ui
+                return null;
             }
+        };
+
+        //bind progress bar to task
+        bar.progressProperty().bind(task.progressProperty());
+        task.setOnRunning(workerStateEvent -> buildNetworkButton.setDisable(true));
+        task.setOnSucceeded(workerStateEvent -> {
+            gridpaneCenter.getChildren().remove(bar);
+            buildNetworkButton.setDisable(false);
         });
-
-    }
-
-    private void parseSmartSystemNetworkJsonData(String sudo) {
-        String ip;
-        String mac;
-        JSONObject smartSystemJson = new JSONObject();
-
-        int deviceCount = 0;
-
-        for (String key : macAddresses.keySet()) {
-
-            if (StringUtils.equals(macAddresses.get(key), ipHostArray[0])) {
-                ip = ipHostArray[0];
-                mac = key;
-                DeviceNode hostNode = new DeviceNodeImpl(ipHostArray[1], ip, mac, sudo, true);
-                System.out.println(hostNode.getNmapScanResponse());
-                smartSystemJson.put(PeepingConstants.HOST_SMART_NODE, hostNode.getSmartDeviceJsonObject());
-                hostNode.voidSudo();
-            } else {
-                ip = macAddresses.get(key);
-                mac = key;
-                DeviceNode deviceNode = new DeviceNodeImpl(PeepingConstants.DEVICE_SMART_NODE + deviceCount,
-                        ip, mac, sudo, false);
-                System.out.println(deviceNode.getNmapScanResponse());
-                smartSystemJson.put(PeepingConstants.DEVICE_SMART_NODE + deviceCount, deviceNode.getSmartDeviceJsonObject());
-                deviceNode.voidSudo();
-                deviceCount = deviceCount + 1;
-            }
-        }
-        smartSystemNetwork = new SmartSystemNetworkImpl(smartSystemJson);
-        System.out.println(smartSystemNetwork.getSmartSystemJSON());
+        new Thread(task).start();
     }
 }
